df3c8fec1aaf9fdcf5c70110106134365da72cf0
Nicholas Chan <nicholaschan1994@hotmail.com>
2018-06-08 17:55:59 -0700

    Create project

---
+++ turtle_graphics/turtle_graphics.js
@@ -0,0 +1,22 @@
+class Turtle{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    forward(step){
+
+    }
+
+    right(){
+
+    }
+
+    left(){
+
+    }
+
+    allPoints(){
+
+    }
+}
