fbb98c1fd30086039a9993aa8a7af3b8dfc70b03
Nicholas Chan <nicholaschan1994@hotmail.com>
2018-06-11 20:02:31 -0700

    Add right(), left() and variables to record the effects of the functions

--- turtle_graphics/turtle_graphics.js
+++ turtle_graphics/turtle_graphics.js
@@ -2,6 +2,9 @@ class Turtle{
     constructor(x, y){
         this.x = x;
         this.y = y;
+        this.hori = true;
+        this.vert = true;
+        this.coor = [];
     }
 
     forward(step){
@@ -9,11 +12,41 @@ class Turtle{
     }
 
     right(){
-
+        if(this.hori === true && this.vert === true){
+            this.hori = false;
+            return this;
+        } else
+        if(this.hori === false && this.vert === false){
+            this.hori = true;
+            return this;
+        } else
+        if(this.hori === true && this.vert === false){
+            this.vert = true;
+            return this;
+        } else
+        if(this.hori === false && this.vert === true){
+            this.vert = false;
+            return this;
+        }
     }
 
     left(){
-
+        if(this.hori === true && this.vert === true){
+            this.vert = false;
+            return this;
+        } else
+        if(this.hori === false && this.vert === false){
+            this.vert = true;
+            return this;
+        } else
+        if(this.hori === true && this.vert === false){
+            this.hori = false;
+            return this;
+        } else
+        if(this.hori === false && this.vert === true){
+            this.hori = true;
+            return this;
+        }
     }
 
     allPoints(){
