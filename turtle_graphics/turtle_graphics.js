52ada3b72dcbb926fde6a6d3271f5cd2198c072e
Nicholas Chan <nicholaschan1994@hotmail.com>
2018-06-14 18:10:24 -0700

    Start forward() and print()

--- turtle_graphics/turtle_graphics.js
+++ turtle_graphics/turtle_graphics.js
@@ -8,7 +8,56 @@ class Turtle{
     }
 
     forward(step){
-
+        if(this.hori === true && this.vert === true){
+            if(!this.coor[this.x]){
+                this.coor[this.x] = [];
+            }
+            let i = 0;
+            while(i <= step){
+                this.coor[this.x][this.y + i] = '@';
+                i++;
+            }
+            return this;
+        } else
+        if(this.hori === false && this.vert === false){
+            if(!this.coor[this.x]){
+                this.coor[this.x] = [];
+            }
+            let i = 0;
+            while(i <= step){
+               this.coor[this.x][this.y-i] = '@';
+               i++;
+            }
+            return this;
+        } else
+        if(this.hori === true && this.vert === false){
+            if(!this.coor[this.x]){
+                this.coor[this.x] = [];
+            }
+            let i = 0;
+            while(i <= step){
+                if(!this.coor[this.x-i]){
+                    this.coor[this.x-i] = [];
+                }
+               this.coor[this.x-i][this.y] = '@';
+               i++;
+            }
+            return this;
+        } else
+        if(this.hori === false && this.vert === true){
+            if(!this.coor[this.x]){
+                this.coor[this.x] = [];
+            }
+            let i = 0;
+            while(i <= step){
+                if(!this.coor[this.x+i]){
+                    this.coor[this.x+i] = [];
+                }
+               this.coor[this.x+i][this.y] = '@';
+               i++;
+            }
+            return this;
+        }
     }
 
     right(){
@@ -49,7 +98,18 @@ class Turtle{
         }
     }
 
-    allPoints(){
+    // allPoints(){
 
+    // }
+
+    print(){
+        for(let i = 0; i < this.coor.length; i++){
+            for(let j = 0; j < this.coor[i].length; j++){
+                if(!this.coor[i][j]){
+                    this.coor[i][j] = ' ';
+                }
+            }
+            console.log(this.coor[i]);
+        }
     }
 }
